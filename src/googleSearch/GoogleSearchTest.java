3fcefb356d3e49dc63be488d441d993be60f2c0e
sjalal <sjalal0228@gmail.com>
2018-01-28 10:26:36 -0600

    added input search verification

--- src/googleSearch/GoogleSearchTest.java
+++ src/googleSearch/GoogleSearchTest.java
@@ -1,7 +1,11 @@
 package googleSearch;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSearchTest {
@@ -12,11 +16,24 @@ public class GoogleSearchTest {
 		System.setProperty("webdriver.chrome.driver", "/Users/apple/Downloads/Browser_driver/chromedriver");
 		// Create ChromeDrive instance
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
 		driver.get("https://www.google.com/");
-		String title = driver.getTitle();
-		System.out.println(title);
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("GitHub");
+		String searchText = element.getAttribute("value");
+		System.out.println(searchText);
 		
-		driver.findElement(By.name("q")).sendKeys("What is github");
+		if(searchText.equals("GitHub")) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		driver.quit();
+		
+		
+		
 	}
 
 }
