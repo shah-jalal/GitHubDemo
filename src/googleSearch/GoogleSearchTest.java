2f4b4ee766dd13717b6ace6a16a9a3276971b991
sjalal <sjalal0228@gmail.com>
2018-01-28 08:42:43 -0600

    inital commit of googleSearch

---
+++ src/googleSearch/GoogleSearchTest.java
@@ -0,0 +1,19 @@
+package googleSearch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+
+	public static void main(String[] args) {
+		// TODO Google Search Test
+
+		System.setProperty("webdriver.chrome.driver", "/Users/apple/Downloads/Browser_driver/chromedriver");
+		// Create ChromeDrive instance
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+}
